c542b5eae7e86a2ca3a61c79b50c0c7c71c6cc5d
Kitsune1904 <annagumiho190423@gmail.com>
2024-08-16 15:06:55 +0300

    add full proj

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import {NavLink} from "react-router-dom";
+import classes from './Navigaton.module.css'
+
+export const Navigation = () => {
+  return (
+    <nav className={classes.navHolder}>
+      <ul>
+        <li><NavLink to={"/"} className={({isActive}) => isActive ? classes.active : ''}>Main</NavLink></li>
+        <li><NavLink to={"/albums"} className={({isActive}) => isActive ? classes.active : ''}>Albums</NavLink></li>
+        <li><NavLink to={"/about"} className={({isActive}) => isActive ? classes.active : ''}>About</NavLink></li>
+      </ul>
+    </nav>
+
+  )
+}
+
